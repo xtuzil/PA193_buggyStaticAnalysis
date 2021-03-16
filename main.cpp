33faf18705cbde0886dd50210dc1b0f8a4fe2426
Matěj Tužil <matej.tuzil@Matejs-MacBook-Pro.local>
2021-03-16 10:21:39 +0100

    codeQL

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int Static[5];
 int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen, int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  
+
   Static[100] = 0;  
   Stack[100] = 0;  
 
